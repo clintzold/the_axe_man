1f49516758e974122af3b89facce25e3b30353e6
clint zold <214383878+clintzold@users.noreply.github.com>
2026-06-21 21:21:59 -0600

    add fallback avatar for testimonials with no image

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,6 +1,5 @@
 class Testimonial < ApplicationRecord
   validates :name, :client_type, :testimony, presence: true
-  validate :image_attached?
 
   has_one_attached :image do |attachable|
     attachable.variant :avatar, resize_to_limit: [200, 200], saver: { strip: true, quality: 70 }, format: :webp
